f1fd710df2386ab29ce6813c4973eb68edbf7864
fahfhi <fah1204fah@gmail..com>
2023-10-23 01:49:05 +0700

    set server side and fetch api for description page

---
+++ server/routes/products.route.js
@@ -0,0 +1,24 @@
+const route = require('express').Router();
+const { add_product, get_product } = require('../controllers/products')
+
+route.post('/add', async (req, res) => {
+    try {
+        const _out = await add_product(req.body);
+        return res.json(_out);
+    } catch(error) {
+        return res.json({ status: false, message: error });
+    }
+})
+
+route.get('/get', async (req, res) => {
+    try {
+        const param = req.query.param;
+        const _out = await get_product(param);
+
+        return res.json(_out)
+    } catch(error) {
+        return res.json({ status: false, message: error });
+    }
+})
+
+module.exports = route;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const cors = require('cors');
 
 const app = express();
 const server = createServer(app);
 
+app.use(cors())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.get('/', (req, res) => {
     res.send('API connected');
+    console.log('API connected')
   });
-  
+
+const products_routes = require('../server/routes/products.route')
+
+app.use('/products', products_routes)
+
 //Run Server -------------------------------------------------
 const PORT = 4000; 
 const SERVER_IP = "localhost"; 
