d7946480c738ebd7ebb2064baec33154fb06a229
kngkanis <kng.kanis@gmail.com>
2023-10-23 17:42:22 +0700

    Homepage 1

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="profile">
+        <img src="/profile-icon.svg" className="profile" />
+      </div>
+      <h1>Bulbee</h1>
+    </div>
+  );
+}
+
+export default Header
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homepage">
+      <div className='banner'>
+        <h1>Banner Area</h1>
+      </div>
+      <div className='banner-indicator'>
+
+      </div>
+      <div className='section'>
+        <div className='my-bulb'>
+          <img src='/light-bulb.png' className='bulb-icon' />
+          <h3>Balance</h3>
+        </div>
+      </div>
+      <div className='category'>
+        <h2>Browse Category</h2>
+      </div>
+    </div>
   )
 }
 
