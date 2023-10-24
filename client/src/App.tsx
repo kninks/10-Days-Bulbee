b73da3d78329257eb27790948fd5997178653636
kngkanis <kng.kanis@gmail.com>
2023-10-24 16:02:01 +0700

    Move CSS to separated files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from './pages/Home'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ client/src/components/Category/Category.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Category.css'
+
+const Category = () => {
+  return (
+    <div className='category-box'>
+        <div className='category-img'>
+            <img />
+        </div>
+        <div className='text-area'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Category
