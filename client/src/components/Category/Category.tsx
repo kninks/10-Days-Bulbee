de89c6800364ea0846fae8840d03c39cca9e2d7d
kngkanis <kng.kanis@gmail.com>
2023-10-24 17:01:25 +0700

    Component done

--- client/src/components/Category/Category.tsx
+++ client/src/components/Category/Category.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import './Category.css'
+import React from "react";
+import "./Category.css";
 
 const Category = () => {
   return (
-    <div className='category-box'>
-        <div className='category-img'>
-            <img />
+    <div className="category-box">
+      <div className="category-img">
+        <img />
+      </div>
+      <div className="text-area">
+        <div className="category-name">
+          <p>All Category</p>
         </div>
-        <div className='text-area'>
-            
+        <div className="item-value">
+          <p>150 items</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import Category from '../components/Category/Category'
+import React from "react";
+import Category from "../components/Category/Category";
 
 const Home = () => {
   return (
     <div className="homepage">
-      <div className='banner'>
+      <div className="banner">
         <h1>Banner Area</h1>
       </div>
-      <div className='banner-indicator'>
-
-      </div>
-      <div className='section'>
-        <div className='my-bulb'>
-          <img src='/light-bulb.png' className='bulb-icon' />
+      <div className="banner-indicator"></div>
+      <div className="section">
+        <div className="my-bulb">
+          <img src="/light-bulb.png" className="bulb-icon" />
           <h3>Balance</h3>
           <h2>10,000</h2>
         </div>
-        <div className='my-purchase'>
-          <img src='/shopping-bag.png' className='bag-icon' />
-          <h2 className='text1'>My</h2>
-          <h2 className='text2'>Purchase</h2>
+        <div className="my-purchase">
+          <img src="/shopping-bag.png" className="bag-icon" />
+          <h2 className="text1">My</h2>
+          <h2 className="text2">Purchase</h2>
         </div>
       </div>
-      <div className='category'>
+      <div className="category-header">
         <h2>Browse Category</h2>
-        <Category />
+      </div>
+      <div className="category-section">
+        <div className="category">
+          <Category />
+          <Category />
+          <Category />
+          <Category />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
