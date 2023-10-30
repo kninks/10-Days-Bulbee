dbd6e453050291df72f0a8b4450ad9ced83027fd
kngkanis <kng.kanis@gmail.com>
2023-10-30 15:44:53 +0700

    Fix all CSS

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./ProductCard.css";
 import LightBulbImg from "../../assets/light-bulb.png";
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;700&display=swap"
+  rel="stylesheet"
+></link>;
 
 interface ProductCardProps {
   id: string;
@@ -17,17 +21,23 @@ const ProductCard: React.FC<ProductCardProps> = ({
   picture_url,
 }) => {
   return (
-    <Link to={`/description?id=${id}`} className="product-card">
-      <div className="img-area">
-        <img src={picture_url} alt={name} />
-      </div>
-      <div className="text-area">
-        <div className="product-name">
-          <p>{name}</p>
-        </div>
-        <div className="product-price">
-          <p>{price}</p>
-          <img src="/light-bulb.png" />
+    <Link to={`/description?id=${id}`} className="product-card-link">
+      <div className="product-card">
+        <img src={picture_url} alt="product pic" />
+        <div className="product-card-des">
+          <div className="product-name">{name}</div>
+          <div className="price-container">
+            <div>
+              <p className="price-number">{price}</p>
+            </div>
+            <div>
+              <img
+                src={LightBulbImg}
+                alt="รูปหลอดไฟจ้า"
+                className="thumbnail"
+              ></img>
+            </div>
+          </div>
         </div>
       </div>
     </Link>
